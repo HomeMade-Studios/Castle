dae168fa022b868939b6743b71d58962030d04da
Romboide82 <ugo98.juve@libero.it>
2015-03-27 20:24:14 +0100

    script

--- Assets/SpawnObject.cs
+++ Assets/SpawnObject.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class SpawnObject : MonoBehaviour {
 
-	public GameObject chair;
-	public GameObject table;
+	static GameObject spawningObject;
+	GameObject spawnedObject;
 	public Camera cam;
 
 	void Update () {
@@ -12,10 +12,19 @@ public class SpawnObject : MonoBehaviour {
 			Spawn();
 		}
 	}
-
+	 
 	void Spawn(){
 		Vector3 spawn=cam.ScreenToWorldPoint (Input.mousePosition);
 		spawn.z=0;
-		Instantiate (chair, spawn, Quaternion.Euler (Vector3.zero));
+		spawnedObject = (GameObject)Instantiate (spawningObject, spawn, Quaternion.Euler (Vector3.zero));
+	}
+
+	public void SaveSpawnedObject(){
+		spawnedObject.GetComponent<Rigidbody2D> ().gravityScale = 50;
+		spawnedObject.GetComponent<Collider2D> ().isTrigger = false;
+	}
+
+	public static void SetSpawningObject( GameObject ObjectToSpawn){
+		spawningObject = ObjectToSpawn;
 	}
 }
