26e5795d6c94b1be97a2997bc2b9ed15561d3bff
Giampli <france.marco@gmail.com>
2015-03-27 17:13:09 +0100

    LeCose

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject : MonoBehaviour {
+
+	public GameObject chair;
+	public GameObject table;
+	public Camera cam;
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Spawn();
+		}
+	}
+
+	void Spawn(){
+		Vector3 spawn=cam.ScreenToWorldPoint (Input.mousePosition);
+		spawn.z=0;
+		Instantiate (chair, spawn, Quaternion.Euler (Vector3.zero));
+	}
+}
