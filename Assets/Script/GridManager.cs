eee7437a34b55b9acf5345a1fe8c3f884deb0fd2
Giampli <france.marco@gmail.com>
2015-04-02 13:21:24 +0200

    WorkingWallSetter

--- Assets/Script/GridManager.cs
+++ Assets/Script/GridManager.cs
@@ -5,15 +5,44 @@ public class GridManager : MonoBehaviour {
 
 	public GameObject Cell;
 	float CellSize=32;
+	public bool BuildMode;
+	public Sprite Wall;
+	public Camera cam;
 
 	void Start () {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
 		Create ();
+	}
+
+	void Update(){
+		if (BuildMode && Input.GetMouseButtonDown (0)) {
+			ChangeWall();
+		}
 	}
 
 	public void Create(){
 		for (int i=0; i<=43; i++) {
 			for (int j=0; j<=22; j++) {
 				Instantiate(Cell,new Vector3(CellSize*i-687,j*CellSize-113),this.transform.rotation);
+			}
+		}
+	}
+
+	public void ChangeWall(){
+		RaycastHit2D tmp= Physics2D.Raycast (cam.ScreenToWorldPoint(Input.mousePosition),Vector2.up);
+		if(tmp.transform.gameObject.tag=="Cell"){
+			if(tmp.transform.gameObject.GetComponent<SpriteRenderer>().sprite==null){
+				tmp.transform.gameObject.GetComponent<SpriteRenderer>().sprite = Wall;
+				print("Lecose");
+			}
+			else{
+				if(tmp.transform.FindChild("Delete").gameObject.activeInHierarchy){
+					tmp.transform.gameObject.GetComponent<SpriteRenderer>().sprite = null;
+					tmp.transform.FindChild("Delete").gameObject.SetActive(false);
+				}
+				else{
+					tmp.transform.FindChild("Delete").gameObject.SetActive(true);
+				}
 			}
 		}
 	}
