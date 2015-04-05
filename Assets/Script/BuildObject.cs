16f9a4763c85615375598cda004ec6b3069dc254
Romboide82 <ugo98.juve@libero.it>
2015-04-05 20:49:44 +0200

    m

---
+++ Assets/Script/BuildObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildObject : MonoBehaviour {
+
+	public int price;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getPrice(){
+		return price;
+	}
+
+}
