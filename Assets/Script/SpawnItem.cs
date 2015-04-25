58c0f4bfb198daf5537aed9e20cc9562814c9e2e
Romboide82 <ugo98.juve@libero.it>
2015-04-25 15:53:09 +0200

    c

---
+++ Assets/Script/SpawnItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject : MonoBehaviour {
+
+	public static GameObject spawningObject;
+	public GameObject spawnedObject;
+	public Camera cam;
+	public static bool canBuild = true;
+
+	void Update () {
+		if (SwitchMode.gameMode == "Build") {
+			if(spawningObject != null && canBuild){
+				if ((Input.GetMouseButtonDown (0)) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)) {
+					Spawn ();
+				}
+			}
+		}
+	}
+	 
+	void Spawn(){
+		Vector3 spawn = cam.ScreenToWorldPoint (Input.mousePosition);
+		spawn.z = 0;
+		Instantiate (spawningObject, spawn, Quaternion.Euler (Vector3.zero));
+	}
+
+	public static void SetSpawningObject( GameObject ObjectToSpawn){
+		spawningObject = ObjectToSpawn;
+	}
+
+}
